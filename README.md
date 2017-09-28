7ceeca05f32059b1f2825f4c6908e2cd8cfc193f
stemado <stemado@outlook.com>
2017-09-28 09:14:43 -0500

    Updated

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ To start Celery:
 Three shells needed:
 
 1. celery -A tb worker -l info (in ptf2 virtualserver and tb directory)
+    a. OR $ celery -A proj beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler 
 2. rabbitmq-server (starts server at local host)
 3. Normal virtual server start-up
--- tb/core/tasks.py
+++ tb/core/tasks.py
@@ -17,6 +17,10 @@ def create_random_user_accounts(total):
         User.objects.create_user(username=username, email=email, password=password)
     return '{} random users created with success!'.format(total)
 
+@shared_task
+def task_test():
+	print('This celery task is running!')
+
 # CREATES RANDOM MEDICATION TIMES (TESTING PURPOSES)
 # @shared_task
 # def create_random_user_accounts(total):
